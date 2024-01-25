661e77b134850c0d9ba2e22cb313ace9dbad081f
Marta Santos <60897114+marta-c-santos@users.noreply.github.com>
2024-01-25 21:04:01 +0000

    Table fixed

--- frontend/src/Views/Home/Home.jsx
+++ frontend/src/Views/Home/Home.jsx
@@ -3,34 +3,36 @@ import axios from "axios";
 import classes from "./Home.module.css";
 
 const TABLE_DATA = [
-    { 
-        id: 1, 
-        organizer: "Associação Académica de Coimbra", 
-        name: "Campeonato interdistrital de Juvenis, Juniores e Seniores PL", 
-        date: "14-07-2023", 
-        state: "active" },
-    { 
-        id: 2, 
-        organizer: "Associação Académica de Coimbra", 
-        name: "Campeonato interdistrital de Juvenis, Juniores e Seniores PL", 
-        date: "14-07-2023", 
-        state: "inactive" },
-    { 
-        id: 3, 
-        organizer: "Associação Académica de Coimbra", 
-        name: "Campeonato interdistrital de Juvenis, Juniores e Seniores PL", 
-        date: "14-07-2023", 
-        state: "inactive" },
-    { 
-        id: 4, 
-        organizer: "Associação Académica de Coimbra", 
-        name: "Campeonato interdistrital de Juvenis, Juniores e Seniores PL", 
-        date: "14-07-2023", 
-        state: "active" },
+    {
+        id: 1,
+        organizer: "Associação Académica de Coimbra",
+        name: "Campeonato interdistrital de Juvenis, Juniores e Seniores PL",
+        date: "14-07-2023",
+        state: "active"
+    },
+    {
+        id: 2,
+        organizer: "Associação Académica de Coimbra",
+        name: "Campeonato interdistrital de Juvenis, Juniores e Seniores PL",
+        date: "14-07-2023",
+        state: "inactive"
+    },
+    {
+        id: 3,
+        organizer: "Associação Académica de Coimbra",
+        name: "Campeonato interdistrital de Juvenis, Juniores e Seniores PL",
+        date: "14-07-2023",
+        state: "inactive"
+    },
+    {
+        id: 4,
+        organizer: "Associação Académica de Coimbra",
+        name: "Campeonato interdistrital de Juvenis, Juniores e Seniores PL",
+        date: "14-07-2023",
+        state: "active"
+    },
     // Add more data as needed
 ];
-
-const TABLE_HEADERS = ["ORGANIZADOR", "NOME", "DATA", "ESTADO"];
 
 function Home(props) {
     let container = classes.container;
@@ -44,13 +46,24 @@ function Home(props) {
             <div className={classes.contentContainer}>
                 <h1>Homepage</h1>
                 <div className={classes.box}>
+                    {/* Title and Buttons */}
+                    <div className={classes.titleContainer}>
+                        <h2>Competições Inscritas</h2>
+                        <div className={classes.titleContainerButtons}>
+                            <input className={classes.button} placeholder="Pesquisar"></input>
+                            <button className={classes.button}>Ordenar por:</button>
+                        </div>
+                    </div>
+
+                    {/* Table */}
                     <div className={classes.table}>
                         <div className={classes.tableRow + " " + classes.tableHeader}>
-                            {TABLE_HEADERS.map((header, index) => (
-                                <div key={index} className={classes.tableCell}>
-                                    {header}
-                                </div>
-                            ))}
+                            <div className={classes.tableRowHeader}>
+                                <div className={`${classes.tableCellHeader} ${classes.organizerColumn}`}>ORGANIZADOR</div>
+                                <div className={`${classes.tableCellHeader} ${classes.nameColumn}`}>NOME</div>
+                                <div className={`${classes.tableCellHeader} ${classes.dateColumn}`}>DATA</div>
+                                <div className={`${classes.tableCellHeader} ${classes.stateColumn}`}>ESTADO</div>
+                            </div>
                         </div>
                         {TABLE_DATA.map((row) => (
                             <div key={row.id} className={classes.tableRow}>
@@ -60,6 +73,12 @@ function Home(props) {
                                 <div className={`${classes.tableCell} ${classes.stateColumn}`}>{row.state}</div>
                             </div>
                         ))}
+                    </div>
+
+                    {/* Page Number and Buttons */}
+                    <div className={classes.pageContainer}>
+                        <span>Page Number</span>
+                        <button className={classes.buttonPages}>Pages</button>
                     </div>
                 </div>
             </div>
