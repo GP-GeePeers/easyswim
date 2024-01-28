e5fc97cec9a16ad9c8fb0114e20c7e36cabec657
Miguel-Soeiro <101994439+Miguel-Soeiro@users.noreply.github.com>
2024-01-28 22:06:26 +0000

    Update urls.py

--- backend/easyswim/urls.py
+++ backend/easyswim/urls.py
@@ -37,6 +37,6 @@ urlpatterns = [
     #path('model-data/', model_data_view, name='model-data-view')
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-
+]
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
