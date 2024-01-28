3a2b9fe1a00715ae7c1282995a7c85daba59a29e
José Aguiar <jpaguiar17@gmail.com>
2024-01-28 06:45:52 +0000

    Update settings.py

--- backend/easyswim/settings.py
+++ backend/easyswim/settings.py
@@ -127,9 +127,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT=os.path.join(BASE_DIR,'assests') 
+
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
