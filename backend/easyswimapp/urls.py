d630b03704277e9111d3440bfa32e03bc369190f
FilipeRibeiro14 <filiperibeiro@student.dei.uc.pt>
2024-01-28 19:53:50 +0000

    add: parse TeamManager and correspondent models

--- backend/easyswimapp/urls.py
+++ backend/easyswimapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import read_lef_view
+from .views import read_lef_view, read_TeamManager_view
 from .views import model_data_view
 
 urlpatterns = [
     path('lxf/', views.LXFView.as_view(), name= 'lxf_list'),
     path('read-lef/', read_lef_view, name='read-lef'),
+    path('read-TeamManager/', read_TeamManager_view, name='read-TeamManager'),
     path('model-data/', model_data_view, name='model-data-view'),
 ]
--- backend/easyswimapp/views.py
+++ backend/easyswimapp/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse
 from .serializers import LXFSerializer
 from .models import LXF
 #from .utils import read_lef_file
-from .utils import read_save_lenex
+from .utils import read_save_lenex, read_save_lenex_TeamManager
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
-from .models import AgeDate,Constructor, Contact_Meet,Contact_Constructor, Meet, Pool, Facility, PointTable, Session, Event, SwimStyle, Fee, AgeGroup
+from .models import AgeDate_MeetManager,Constructor_MeetManager, Contact_Meet_MeetManager,Contact_Constructor_MeetManager, Meet_MeetManager, Pool_MeetManager, Facility_MeetManager, PointTable_MeetManager, Session_MeetManager, Event_MeetManager, SwimStyle_MeetManager, Fee_MeetManager, AgeGroup_MeetManager
 from django.http import JsonResponse
 from django.conf import settings
 import os
@@ -48,29 +48,29 @@ class LXFView(APIView):
 
 def read_lef_view(request):
     
-    #file_path = '/easyswim/backend/media/lef_files/Meet_20230926151151490_4.lef'
+   
     file_path = os.path.join(settings.MEDIA_ROOT, 'lef_files', 'test.lef')
-    print("Caminho: "+file_path)
+    print("Path: "+file_path)
     try:
         read_save_lenex(file_path)
-        return HttpResponse('Arquivo .lef lido e processado com sucesso.')
+        return HttpResponse('.lef file read and processed successfully.')
     except Exception as e:
-        return HttpResponse(f'Ocorreu um erro ao processar o arquivo .lef: {e}')
+        return HttpResponse(f'An error occurred while processing the .lef file: {e}')
     
 def model_data_view(request):
 
-    meets = list(Meet.objects.values())  # Converte QuerySet em uma lista de dicionários
-    events = list(Event.objects.values())
-    cons = list(Constructor.objects.values())   # Converte QuerySet em uma lista de dicionários
-    cont_constructor = list(Contact_Constructor.objects.values())
-    cont_meet = list(Contact_Meet.objects.values())
-    pool = list(Pool.objects.values())
-    facility = list(Facility.objects.values())
-    pointtable = list(PointTable.objects.values())
-    session = list(Session.objects.values())
-    swimstyle = list(SwimStyle.objects.values())
-    fee = list(Fee.objects.values())
-    agegroup = list(AgeGroup.objects.values())
+    meets = list(Meet_MeetManager.objects.values())  
+    events = list(Event_MeetManager.objects.values())
+    cons = list(Constructor_MeetManager.objects.values())   
+    cont_constructor = list(Contact_Constructor_MeetManager.objects.values())
+    cont_meet = list(Contact_Meet_MeetManager.objects.values())
+    pool = list(Pool_MeetManager.objects.values())
+    facility = list(Facility_MeetManager.objects.values())
+    pointtable = list(PointTable_MeetManager.objects.values())
+    session = list(Session_MeetManager.objects.values())
+    swimstyle = list(SwimStyle_MeetManager.objects.values())
+    fee = list(Fee_MeetManager.objects.values())
+    agegroup = list(AgeGroup_MeetManager.objects.values())
    
 
     data = {
@@ -90,3 +90,14 @@ def model_data_view(request):
     }
     print(meets)
     return JsonResponse(data)
+
+
+def read_TeamManager_view(request):
+    
+    file_path = os.path.join(settings.MEDIA_ROOT, 'lef_files', 'teamManager.lef')
+    print("Path: "+file_path)
+    try:
+        read_save_lenex_TeamManager(file_path)
+        return HttpResponse('.lef file read and processed successfully.')
+    except Exception as e:
+        return HttpResponse(f'An error occurred while processing the .lef file: {e}')
