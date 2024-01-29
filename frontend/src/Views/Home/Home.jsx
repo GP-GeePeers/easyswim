afc858a677ae48c16e3ba30d453763dfbdffc7bd
João Catré <joaomcatre@hotmail.com>
2024-01-29 23:38:41 +0000

    Update Home.jsx
    Co-Authored-By: Marta Santos <60897114+marta-c-santos@users.noreply.github.com>

--- frontend/src/Views/Home/Home.jsx
+++ frontend/src/Views/Home/Home.jsx
@@ -8,21 +8,21 @@ const mockDataList = [
     {
         id: 1,
         organizer: "Associação Académica de Coimbra",
-        name: "Campeonato interdistrital de Juvenis, Juniores e Seniores PL",
+        name: "A - Campeonato interdistrital de Juvenis, Juniores e Seniores PL",
         date: "15-07-2024",
         state: "active",
     },
     {
         id: 2,
         organizer: "Associação Académica de Coimbra",
-        name: "ACampeonato interdistrital de Juvenis, Juniores e Seniores PL",
+        name: "B - Campeonato interdistrital de Juvenis, Juniores e Seniores PL",
         date: "14-06-2024",
         state: "inactive",
     },
     {
         id: 3,
         organizer: "Associação Académica de Coimbra",
-        name: "RCampeonato interdistrital de Juvenis, Juniores e Seniores PL",
+        name: "C - Campeonato interdistrital de Juvenis, Juniores e Seniores PL",
         date: "16-05-2024",
         state: "active",
     },
@@ -36,85 +36,78 @@ function Home(props) {
     // console.log(mockTableDataList);
     const [mockNextCompetition, setMockNextCompetition] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(
-                    "http://127.0.0.1:8000/api/meets-data/"
-                );
-                const data = await response.json();
-                const currentDate = new Date();
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(
+                "http://127.0.0.1:8000/api/meets-data/"
+            );
+            const data = await response.json();
+            const currentDate = new Date();
 
-                const updatedTableData = data.meets.map((meet) => {
-                    const meetDate = new Date(meet.deadline);
-                    const isActive = meetDate >= currentDate;
+            const updatedTableData = data.meets.map((meet) => {
+                const meetDate = new Date(meet.deadline);
+                const isActive = meetDate >= currentDate;
 
-                    return {
-                        id: meet.id,
-                        organizer: meet.organizer,
-                        name: meet.name,
-                        date: meet.deadline,
-                        state: isActive ? "active" : "inactive",
-                    };
-                });
+                return {
+                    id: meet.id,
+                    organizer: meet.organizer,
+                    name: meet.name,
+                    date: meet.deadline,
+                    state: isActive ? "active" : "inactive",
+                };
+            });
 
-                setTableData(updatedTableData);
-            } catch (error) {
-                console.error("Error:", error);
-            }
-        };
-        // fetchData();
-    }, []);
-
-    // useEffect(() => {
-    const getNextCompetition = () => {
-        console.log(1);
-        if (!mockTableDataList || !Array.isArray(mockTableDataList)) {
-            console.log(2);
-
-            setMockNextCompetition(null);
-            // setNextCompetitionData(null);
+            setTableData(updatedTableData);
+        } catch (error) {
+            console.error("Error:", error);
         }
+    };
 
+    const getNextCompetition = () => {
+        if (!mockTableDataList || !Array.isArray(mockTableDataList)) {
+            setMockNextCompetition(null);
+        }
         const currentDate = new Date();
-
         for (const meet of mockTableDataList) {
-            console.log(meet);
             if (!meet || typeof meet !== "object" || !meet.date) {
-                console.log(3);
                 continue;
             }
-
             const [day, month, year] = meet.date.split("-").map(Number);
-
             const meetDate = new Date(year, month - 1, day);
-            console.log(meetDate);
-
             if (meet.state === "active" && meetDate >= currentDate) {
-                console.log(4);
-
                 setMockNextCompetition(meet);
-                // setNextCompetitionData(meet);
             }
         }
     };
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, []);
+    // const getNextCompetition = () => {
+    //     if (!mockTableDataList || !Array.isArray(mockTableDataList)) {
+    //         setNextCompetitionData(null);
+    //     }
+    //     const currentDate = new Date();
+    //     for (const meet of mockTableDataList) {
+    //         if (!meet || typeof meet !== "object" || !meet.date) {
+    //             continue;
+    //         }
+    //         const [day, month, year] = meet.date.split("-").map(Number);
+    //         const meetDate = new Date(year, month - 1, day);
+    //         if (meet.state === "active" && meetDate >= currentDate) {
+    //             setNextCompetitionData(meet);
+    //         }
+    //     }
+    // };
 
     useEffect(() => {
+        fetchData();
         getNextCompetition();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-
-    useEffect(() => {
-        console.log(mockNextCompetition);
-    }, [mockNextCompetition]); // Adding mockNextCompetition as a dependency
 
     return (
         <div className={classes.topCardContainer}>
             <NextCompetition
                 changeCompDetailsModal={props.changeCompDetailsModal}
-                // nextCompetitionData={nextCompetitionData}
+                nextCompetitionData={nextCompetitionData}
                 mockNextCompetition={mockNextCompetition}
             />
             <CompetitionsList
