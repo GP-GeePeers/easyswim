14b188aa994fecf3ada55f90c06a1c0128a5cf12
DiogoCleto1999 <116749736+DiogoCleto1999@users.noreply.github.com>
2024-01-29 11:09:49 +0000

    requirements fix

--- backend/easyswim/settings.py
+++ backend/easyswim/settings.py
@@ -46,9 +46,7 @@ INSTALLED_APPS = [
     "easyswimapp",
     "corsheaders",
     "djoser",
-    'rest_framework_simplejwt',
-    'rest_framework_simplejwt.token_blacklist'
-    
+    'rest_framework_simplejwt'    
     
 ]
 
