4dac13205bd5355f7d3515f6012f4419d43f6ecf
João Catré <joaomcatre@hotmail.com>
2024-01-30 08:02:57 +0000

    backup
    Co-Authored-By: Marta Santos <60897114+marta-c-santos@users.noreply.github.com>

--- frontend/src/Components/Cards/CompetitionsList/CompetitionsList.jsx
+++ frontend/src/Components/Cards/CompetitionsList/CompetitionsList.jsx
@@ -1,48 +1,57 @@
 import React, { useEffect, useState } from "react";
 import classes from "./CompetitionsList.module.css";
 import Card from "../Card";
+import Button from "../../Buttons/Button";
 
-const mockData = [
-    {
-        id: 1,
-        name: "Campeonato interdistrital de Juvenis, Juniores e Seniores PL",
-        local: "Coimbra",
-        organizer: "Associação Académica de Coimbra",
-        number: "230714",
-        date: "14-07-2023",
-        course_length: "25 meters (SCM)",
-        register_limit_date: "13-07-2023 | 09:00",
-        state: "active",
-    },
-];
-
-const ORDER_OPTIONS = ["Mais recente", "Mais antigo", "Nome", "Data"];
+const ORDER_OPTIONS = ["Mais recente", "Mais antigo", "Nome"];
+const TABLE_HEADERS = ["ORGANIZADOR", "NOME", "DATA", "ESTADO"];
 
 function CompetitionsList(props) {
     // TODO - check string sizes and add "..." if too big, just like made in src/App.jsx
+    const [searchInput, setSearchInput] = useState("");
     const [selectedOrder, setSelectedOrder] = useState("Mais recente");
-    const keys = Object.keys(mockData[0]);
-
-    const [contentHeights, setContentHeights] = useState(
-        Array(keys.length).fill(0)
-    );
-
-    useEffect(() => {
-        const newContentHeights = contentHeights.map((_, index) => {
-            const contentRef = document.getElementById(`content-${index}`);
-            return contentRef ? contentRef.clientHeight : 0;
-        });
-
-        setContentHeights(newContentHeights);
-    }, [props.compDetailsModal, window.innerWidth]); // eslint-disable-line react-hooks/exhaustive-deps
-
-    const capitalizeFirstLetter = (str) => {
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    };
 
     const handleOrderOptionClick = (option) => {
+        props.setMockTableDataList(
+            props.mockDataList
+                .filter(
+                    (row) =>
+                        row.organizer
+                            .toLowerCase()
+                            .includes(searchInput.toLowerCase()) ||
+                        row.name
+                            .toLowerCase()
+                            .includes(searchInput.toLowerCase())
+                )
+                .sort((a, b) => {
+                    console.log(option);
+                    let returnDate;
+                    if (option === "Mais antigo") {
+                        if (a.date < b.date) {
+                            returnDate = -1;
+                        } else if (a.date > b.date) {
+                            returnDate = 1;
+                        } else {
+                            returnDate = 0;
+                        }
+                        return returnDate;
+                    } else if (option === "Nome") {
+                        return a.name.localeCompare(b.name);
+                    } else {
+                        if (a.date < b.date) {
+                            returnDate = 1;
+                        } else if (a.date > b.date) {
+                            returnDate = -1;
+                        } else {
+                            returnDate = 0;
+                        }
+                        return returnDate;
+                    }
+                })
+        );
         setSelectedOrder(option);
     };
+
     return (
         <>
             <Card>
@@ -53,6 +62,10 @@ function CompetitionsList(props) {
                         </div>
                     </div>
                     <div className={classes.buttonsContainer}>
+                        <input
+                            className={classes.buttonSearch}
+                            placeholder="Pesquisar"
+                        ></input>
                         <div className={classes.dropdown}>
                             <div className={classes.buttonDropdown}>
                                 <div className={classes.text}>
@@ -72,61 +85,95 @@ function CompetitionsList(props) {
                                 ))}
                             </div>
                         </div>
-                        <input
-                            className={classes.buttonSearch}
-                            placeholder="Pesquisar"
-                        ></input>
                     </div>
                 </div>
-                <div className={classes.tableContainer}>
-                    {keys.map(
-                        (key, index) =>
-                            // Exclude "id" from rendering
-                            key !== "id" && (
-                                <React.Fragment key={key}>
-                                    <div className={classes.contentContainer}>
-                                        <div
-                                            className={
-                                                classes.contentTitleContainer
-                                            }
-                                        >
-                                            <div className={classes.category}>
-                                                {capitalizeFirstLetter(
-                                                    key.replace(/_/g, " ")
-                                                )}
-                                            </div>
-                                        </div>
-                                        <div
-                                            className={classes.verticalLine}
-                                            style={{
-                                                height: `${contentHeights[index]}px`,
-                                            }}
-                                        />
-                                        <div
-                                            className={
-                                                classes.contentDetailsContainer
-                                            }
-                                            id={`content-${index}`}
-                                        >
-                                            <div className={classes.details}>
-                                                {mockData.map((data) => (
-                                                    <React.Fragment
-                                                        key={data[key]}
-                                                    >
-                                                        {data[key]}
-                                                    </React.Fragment>
-                                                ))}
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {index < keys.length - 1 && (
-                                        <div
-                                            className={classes.horizontalLine}
-                                        />
-                                    )}
-                                </React.Fragment>
-                            )
-                    )}
+                <div className={classes.table}>
+                    <div
+                        className={classes.tableRow + " " + classes.tableHeader}
+                    >
+                        <div className={classes.tableRowHeader}>
+                            <div
+                                className={
+                                    classes.tableCellHeader +
+                                    " " +
+                                    classes.organizerColumn
+                                }
+                            >
+                                ORGANIZADOR
+                            </div>
+                            <div
+                                className={
+                                    classes.tableCellHeader +
+                                    " " +
+                                    classes.nameColumn
+                                }
+                            >
+                                NOME
+                            </div>
+                            <div
+                                className={
+                                    classes.tableCellHeader +
+                                    " " +
+                                    classes.dateColumn
+                                }
+                            >
+                                DATA
+                            </div>
+                            <div
+                                className={
+                                    classes.tableCellHeader +
+                                    " " +
+                                    classes.stateColumn
+                                }
+                            >
+                                ESTADO
+                            </div>
+                        </div>
+                    </div>
+                    {props.mockDataList?.map((row) => (
+                        <div key={row.id} className={classes.tableRow}>
+                            <div
+                                className={
+                                    classes.tableCell +
+                                    " " +
+                                    classes.organizerColumn
+                                }
+                            >
+                                {row.organizer}
+                            </div>
+                            <div
+                                className={
+                                    classes.tableCell + " " + classes.nameColumn
+                                }
+                            >
+                                {row.name}
+                            </div>
+                            <div
+                                className={
+                                    classes.tableCell + " " + classes.dateColumn
+                                }
+                            >
+                                {row.date}
+                            </div>
+                            <div
+                                className={
+                                    classes.tableCell +
+                                    " " +
+                                    classes.stateColumn
+                                }
+                            >
+                                {row.state === "active" ? (
+                                    <Button text={row.state} disabled={true} />
+                                ) : (
+                                    <Button
+                                        text={row.state}
+                                        type="secondary"
+                                        disabled={true}
+                                    />
+                                )}
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </Card>
         </>
